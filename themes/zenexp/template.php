946334f3e36710673b192fdfc8601e2ef85ad867
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-10-02 01:50:45 +0700

    Add pager and pager-mini support.

--- themes/zenexp/template.php
+++ themes/zenexp/template.php
@@ -7,126 +7,270 @@
  * @see https://drupal.org/node/1728096
  */
 
+function zenexp_breadcrumb($variables) {
+	$breadcrumb = $variables ['breadcrumb'];
+	$output = '';
 
-/**
- * Override or insert variables into the maintenance page template.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("maintenance_page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
-  // When a variable is manipulated or added in preprocess_html or
-  // preprocess_page, that same work is probably needed for the maintenance page
-  // as well, so we can just re-use those functions to do that work here.
-  STARTERKIT_preprocess_html($variables, $hook);
-  STARTERKIT_preprocess_page($variables, $hook);
+	// Determine if we are to display the breadcrumb.
+
+	array_shift ( $breadcrumb );
+
+	// Return the breadcrumb with separators.
+	if (! empty ( $breadcrumb )) {
+		$trailing_separator = $title = '';
+		$item = menu_get_item ();
+		if (! empty ( $item ['tab_parent'] )) {
+			// If we are on a non-default tab, use the tab's title.
+			$breadcrumb [] = check_plain ( $item ['title'] );
+		} else {
+			$breadcrumb [] = drupal_get_title ();
+		}
+
+		// Provide a navigational heading to give context for breadcrumb links to
+		// screen-reader users.
+		if (empty ( $variables ['title'] )) {
+			$variables ['title'] = t ( 'You are here' );
+		}
+		// Unless overridden by a preprocess function, make the heading invisible.
+		if (! isset ( $variables ['title_attributes_array'] ['class'] )) {
+			$variables ['title_attributes_array'] ['class'] [] = 'element-invisible';
+		}
+
+		// Build the breadcrumb trail.
+		$output = '<nav class="breadcrumb" role="navigation">';
+		$output .= '<h2' . drupal_attributes ( $variables ['title_attributes_array'] ) . '>' . $variables ['title'] . '</h2>';
+		$output .= '<ol class="breadcrumb"><li>' . implode ( '</li><li>', $breadcrumb ) . $trailing_separator . '</li></ol>';
+		$output .= '</nav>';
+	}
+	return $output;
 }
-// */
+
 
 /**
- * Override or insert variables into the html templates.
+ * Returns HTML for a query pager.
+ *
+ * Menu callbacks that display paged query results should call theme('pager') to
+ * retrieve a pager control so that users can view other results. Format a list
+ * of nearby pages with additional query results.
  *
  * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("html" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_html(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // The body tag's classes are controlled by the $classes_array variable. To
-  // remove a class from $classes_array, use array_diff().
-  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
-}
-// */
-
-/**
- * Override or insert variables into the page templates.
+ *   An associative array containing:
+ *   - tags: An array of labels for the controls in the pager.
+ *   - element: An optional integer to distinguish between multiple pagers on
+ *     one page.
+ *   - parameters: An associative array of query string parameters to append to
+ *     the pager links.
+ *   - quantity: The number of pages in the list.
  *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("page" in this case.)
+ * @ingroup themeable
+ * @see theme_pager
  */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
+function zenexp_pager($variables) {
+  $tags = $variables['tags'];
+  $element = $variables['element'];
+  $parameters = $variables['parameters'];
+  $quantity = $variables['quantity'];
+  global $pager_page_array, $pager_total;
 
-/**
- * Override or insert variables into the node templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("node" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_node(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+  // Calculate various markers within this pager piece:
+  // Middle is used to "center" pages around the current page.
+  $pager_middle = ceil($quantity / 2);
+  // current is the page we are currently paged to
+  $pager_current = $pager_page_array[$element] + 1;
+  // first is the first page listed by this pager piece (re quantity)
+  $pager_first = $pager_current - $pager_middle + 1;
+  // last is the last page listed by this pager piece (re quantity)
+  $pager_last = $pager_current + $quantity - $pager_middle;
+  // max is the maximum page number
+  $pager_max = $pager_total[$element];
+  // End of marker calculations.
 
-  // Optionally, run node-type-specific preprocess functions, like
-  // STARTERKIT_preprocess_node_page() or STARTERKIT_preprocess_node_story().
-  $function = __FUNCTION__ . '_' . $variables['node']->type;
-  if (function_exists($function)) {
-    $function($variables, $hook);
+  // Prepare for generation loop.
+  $i = $pager_first;
+  if ($pager_last > $pager_max) {
+    // Adjust "center" if at end of query.
+    $i = $i + ($pager_max - $pager_last);
+    $pager_last = $pager_max;
+  }
+  if ($i <= 0) {
+    // Adjust "center" if at start of query.
+    $pager_last = $pager_last + (1 - $i);
+    $i = 1;
+  }
+  // End of generation loop preparation.
+
+  $li_first = theme('pager_first', array('text' => (isset($tags[0]) ? $tags[0] : t('« first')), 'element' => $element, 'parameters' => $parameters));
+  $li_previous = theme('pager_previous', array('text' => (isset($tags[1]) ? $tags[1] : t('‹ previous')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
+  $li_next = theme('pager_next', array('text' => (isset($tags[3]) ? $tags[3] : t('next ›')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
+  $li_last = theme('pager_last', array('text' => (isset($tags[4]) ? $tags[4] : t('last »')), 'element' => $element, 'parameters' => $parameters));
+
+  if ($pager_total[$element] > 1) {
+    if ($li_first) {
+      $items[] = array(
+        'class' => array('first'),
+        'data' => $li_first,
+      );
+    }
+    if ($li_previous) {
+      $items[] = array(
+        'class' => array('previous'),
+        'data' => $li_previous,
+      );
+    }
+
+    // When there is more than one page, create the pager list.
+    if ($i != $pager_max) {
+      if ($i > 1) {
+        $items[] = array(
+          'class' => array('disabled'),
+          'data' => '<span>…</span>',
+        );
+      }
+      // Now generate the actual pager piece.
+      for (; $i <= $pager_last && $i <= $pager_max; $i++) {
+        if ($i < $pager_current) {
+          $items[] = array(
+            'data' => theme('pager_previous', array('text' => $i, 'element' => $element, 'interval' => ($pager_current - $i), 'parameters' => $parameters)),
+          );
+        }
+        if ($i == $pager_current) {
+          $items[] = array(
+            'class' => array('active'),
+            'data' => "<span>$i</span>",
+          );
+        }
+        if ($i > $pager_current) {
+          $items[] = array(
+            
+            'data' => theme('pager_next', array('text' => $i, 'element' => $element, 'interval' => ($i - $pager_current), 'parameters' => $parameters)),
+          );
+        }
+      }
+      if ($i < $pager_max) {
+        $items[] = array(
+          'class' => array('disabled'),
+          'data' => '<span>…</span>',
+        );
+      }
+    }
+    // End generation.
+    if ($li_next) {
+      $items[] = array(
+        'class' => array('pager-next'),
+        'data' => $li_next,
+      );
+    }
+    if ($li_last) {
+      $items[] = array(
+        'class' => array('last'),
+        'data' => $li_last,
+      );
+    }
+    return '<h2 class="element-invisible">' . t('Pages') . '</h2>' . '<div class="clearfix text-center">' . theme('item_list', array(
+      'items' => $items,
+      'class' => array('center'),
+      'attributes' => array('class' => array('pagination')),
+    )) . '</div>';
   }
 }
-// */
+
 
 /**
- * Override or insert variables into the comment templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("comment" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_comment(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
-// */
+* Render the pager_mini in views as a pager, not a pagination.
+* ← Previous    																			Next →
 
-/**
- * Override or insert variables into the region templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("region" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_region(&$variables, $hook) {
-  // Don't use Zen's region--sidebar.tpl.php template for sidebars.
-  //if (strpos($variables['region'], 'sidebar_') === 0) {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
-  //}
-}
-// */
 
-/**
- * Override or insert variables into the block templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("block" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_block(&$variables, $hook) {
-  // Add a count to all the blocks in the region.
-  // $variables['classes_array'][] = 'count-' . $variables['block_id'];
+**/
+function zenexp_views_mini_pager($vars) {
+  global $pager_page_array, $pager_total;
 
-  // By default, Zen will use the block--no-wrapper.tpl.php for the main
-  // content. This optional bit of code undoes that:
-  //if ($variables['block_html_id'] == 'block-system-main') {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
-  //}
+  $tags = $vars['tags'];
+  $element = $vars['element'];
+  $parameters = $vars['parameters'];
+
+  // current is the page we are currently paged to
+  $pager_current = $pager_page_array[$element] + 1;
+  // max is the maximum page number
+  $pager_max = $pager_total[$element];
+  // End of marker calculations.
+
+  if ($pager_total[$element] > 1) {
+
+    $li_previous = theme('pager_previous',
+      array(
+        'text' => ($text = (isset($tags[1]) ? $tags[1] : t('←'))),
+        'element' => $element,
+        'interval' => 1,
+        'parameters' => $parameters,
+      )
+    );
+    if (empty($li_previous)) {
+      $items[] = array(
+		    'class' => array('previous disabled'),
+		    'data' => "<span>$text</span>",
+		  );
+    } else {
+      $items[] = array(
+		    'class' => array('previous'),
+		    'data' => $li_previous,
+		  );
+    }
+
+    $li_next = theme('pager_next',
+      array(
+        'text' => ($text = (isset($tags[3]) ? $tags[3] : t('→'))),
+        'element' => $element,
+        'interval' => 1,
+        'parameters' => $parameters,
+      )
+    );
+
+    $items[] = array(
+      'class' => array('active'),
+      'data' => '<span>' . t('@current / @max', array('@current' => $pager_current, '@max' => $pager_max)) . '</span>',
+    );
+
+    if (empty($li_next)) {
+      $items[] = array(
+		    'class' => array('next disabled'),
+		    'data' => "<span>$text</span>",
+		  );
+    } else {
+      $items[] = array(
+		    'class' => array('next'),
+		    'data' => $li_next,
+		  );
+    }
+    
+    return '<div class="text-center clearfix">' . theme('item_list',
+      array(
+        'items' => $items,
+        'title' => NULL,
+        'type' => 'ul',
+        'attributes' => array('class' => array('pager')),
+      )
+    ) . '</div>';
+  }
 }
-// */
+
+
+/* Taken from Tweme. Dont know why it not work... 
+function phptemplate_item_list($vars) {
+  if (isset($vars['attributes']['class']) && in_array('pager', $vars['attributes']['class'])) {
+    unset($vars['attributes']['class']);
+    foreach ($vars['items'] as $i => &$item) {
+      if (in_array('pager-current', $item['class'])) {
+        $item['class'] = array('active');
+        $item['data'] = '<a href="#">' . $item['data'] . '</a>';
+      }
+      elseif (in_array('pager-ellipsis', $item['class'])) {
+        $item['class'] = array('disabled');
+        $item['data'] = '<a href="#">' . $item['data'] . '</a>';
+      }
+    }
+    return '<div class="pagination pagination-centered">' . theme_item_list($vars) . '</div>';
+  }
+  return theme_item_list($vars);
+}
+
+
